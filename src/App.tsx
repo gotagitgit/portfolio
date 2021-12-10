c9d64fc077e2c1694d3f6418b0bab8ba7df6205e
Cyrus Penuliar <Cyrus.Penuliar@infor.com>
2021-12-10 22:45:35 +0800

    adding styles, images and svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import './styles.css'
+import IMAGE from './react.png'
+import LOGO from './react.svg'
 
-const App = () => <div>Hello React App from scratch</div>;
-
-export default App;
+export const App = () =>
+{
+    return <>
+        <h1>Hello React App from scratch</h1>
+        <img src={IMAGE} alt="React Logo" width="300" height="200"/>
+        <img src={LOGO} alt="React Logo" width="300" />
+    </>
+} 
