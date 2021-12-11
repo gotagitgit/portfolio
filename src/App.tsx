256493ffd05686023188a5cd52948c1cb0eec528
Cyrus Penuliar <Cyrus.Penuliar@infor.com>
2021-12-11 10:46:03 +0800

    Add prettier formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React from 'react'
 import './styles.css'
 import IMAGE from './react.png'
 import LOGO from './react.svg'
-import { Counter } from "./Counter";
+import { Counter } from './Counter'
 
-export const App = () =>
-{
-    return <>
-        <h1>Hello React App from scratch - {process.env.NODE_ENV} {process.env.name}</h1>
-        <img src={IMAGE} alt="React Logo" width="300" height="200"/>
-        <img src={LOGO} alt="React Logo" width="300" />
-        <Counter />
+export const App = () => {
+  return (
+    <>
+      <h1>
+        Hello React App from scratch - {process.env.NODE_ENV} {process.env.name}
+      </h1>
+      <img src={IMAGE} alt="React Logo" width="300" height="200" />
+      <img src={LOGO} alt="React Logo" width="300" />
+      <Counter />
     </>
-} 
+  )
+}
