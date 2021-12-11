36677a7004840edf623e8e822b793a4dc70af5d6
Cyrus Penuliar <Cyrus.Penuliar@infor.com>
2021-12-11 09:46:59 +0800

    hot reload with react refresh plugin

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import './styles.css'
 import IMAGE from './react.png'
 import LOGO from './react.svg'
+import { Counter } from "./Counter";
 
 export const App = () =>
 {
@@ -9,5 +10,6 @@ export const App = () =>
         <h1>Hello React App from scratch - {process.env.NODE_ENV} {process.env.name}</h1>
         <img src={IMAGE} alt="React Logo" width="300" height="200"/>
         <img src={LOGO} alt="React Logo" width="300" />
+        <Counter />
     </>
 } 
